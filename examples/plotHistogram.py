cc5a06abc770c9f8efddb61ef091b8ea10ff9c1e
rowen <rowen@git.lsstcorp.org>
2009-11-06 20:59:05 +0000

    Tweaked example policy for simDeep amplifiers (0th order kernel) Tweaked plotHistogram to reject outliers

--- examples/plotHistogram.py
+++ examples/plotHistogram.py
@@ -14,63 +14,86 @@ NBins = 500
 UseLogForY = False
 UseSqrtForX = False
 
+
+def clipOutliers(arr):
+    """Clip values 3 sigma outside the median
+    where sigma is estimated as the inter-quartile range * 0.741
+    """
+    arr.sort()
+    arrLen = len(arr)
+    iqr = arr[arrLen * 3 / 4] - arr[arrLen / 4]
+    threeSigma = 4 * iqr * 0.741
+    median = arr[arrLen / 2]
+    minGood = median - threeSigma
+    maxGood = median + threeSigma
+    return numpy.extract((arr >= minGood) & (arr <= maxGood), arr)
+
+def plotHistogram(coaddName, chiSqOrder):
+    coadd = pyfits.open(coaddName)
+    coaddData = coadd[0].data
+    # undo normalization
+    coaddData *= float(chiSqOrder)
+    # get rid of nans and infs
+    goodData = numpy.extract(numpy.isfinite(coaddData.flat), coaddData.flat)
+    # clip outliers
+    for ii in range(2):
+        goodData = clipOutliers(goodData)
+    
+    hist, binEdges = numpy.histogram(goodData, bins=NBins)
+    if UseLogForY:
+        dataY = numpy.log10(numpy.where(hist > 1.0, hist, 1.0))
+    else:
+        dataY = hist
+    
+    dataX = binEdges[0:-1]
+    if UseSqrtForX:
+        plotDataX = numpy.sqrt(dataX)
+    else:
+        plotDataX = dataX
+    
+    # plot histogram: log10(frequency) vs. value
+    pyplot.plot(plotDataX, dataY, drawstyle="steps")
+    if UseLogForY:
+        pyplot.ylabel('log10 frequency')
+    else:
+        pyplot.ylabel('frequency')
+    if UseSqrtForX:
+        pyplot.xlabel('sqrt of sum of (counts/noise)^2')
+    else:
+        pyplot.xlabel('sum of (counts/noise)^2')
+
+    # plot chiSq probability distribution
+    chiSqX = dataX
+    chiSqDist = numpy.power(chiSqX, (chiSqOrder / 2.0) - 1) * numpy.exp(-chiSqX / 2.0)
+    
+    # fit scale by fitting points up to peak in histogram (crude but it's a start)
+    maxPos = hist.argmax()
+    goodVals = hist[0:maxPos] > 1
+    scaleFactorArr = numpy.extract(goodVals, hist[0:maxPos] / chiSqDist[0:maxPos])
+    for ii in range(2):
+        scaleFactorArr = clipOutliers(scaleFactorArr)
+    scaleFactor = numpy.median(scaleFactorArr)
+    chiSqDist *= scaleFactor
+    if UseLogForY:
+        chiSqDistY = numpy.log10(numpy.where(chiSqDist > 1.0, chiSqDist, 1.0))
+    else:
+        chiSqDistY = chiSqDist
+    pyplot.plot(plotDataX, chiSqDistY)
+    
+    pyplot.show()
+
 if __name__ == "__main__":
-    helpStr = """Usage: plotHistogram.py coaddfile nImages
+    helpStr = """Usage: plotHistogram.py coaddfile chiSqOrder
 
 where:
 - coaddfile is the path of the coadd
+- chiSqOrder is the chi squared order for the best fit line; typically the # of images
 """
     if len(sys.argv) != 3:
         print helpStr
         sys.exit(0)
     
     coaddName = sys.argv[1]
-    nImages = int(sys.argv[2])
-    
-coadd = pyfits.open(coaddName)
-coaddData = coadd[0].data
-# undo normalization
-coaddData *= float(nImages)
-# get rid of nans and take square root
-goodData = numpy.extract(numpy.isfinite(coaddData.flat), coaddData.flat)
-hist, binEdges = numpy.histogram(goodData, bins=NBins)
-if UseLogForY:
-    dataY = numpy.log10(numpy.where(hist > 1.0, hist, 1.0))
-else:
-    dataY = hist
+    chiSqOrder = float(sys.argv[2])
 
-dataX = binEdges[0:-1]
-if UseSqrtForX:
-    plotDataX = numpy.sqrt(dataX)
-else:
-    plotDataX = dataX
-
-# plot log10(frequency) vs. sqrt of value
-pyplot.plot(plotDataX, dataY, drawstyle="steps")
-if UseLogForY:
-    pyplot.ylabel('log10 frequency')
-else:
-    pyplot.ylabel('frequency')
-if UseSqrtForX:
-    pyplot.xlabel('sqrt of sum of (counts/noise)^2')
-else:
-    pyplot.xlabel('sum of (counts/noise)^2')
-
-# plot chiSq probability distribution
-chiSqX = dataX
-chiSqDist = numpy.power(chiSqX, (nImages / 2.0) - 1) * numpy.exp(-chiSqX / 2.0)
-
-# fit scale by fitting points up to peak in histogram (crude but it's a start)
-maxPos = hist.argmax()
-goodVals = hist[0:maxPos] > 1
-scaleFactors = numpy.extract(goodVals, hist[0:maxPos] / chiSqDist[0:maxPos])
-weights = numpy.extract(goodVals, hist[0:maxPos])
-meanScaleFactor = numpy.average(scaleFactors, weights=weights)
-chiSqDist *= meanScaleFactor
-if UseLogForY:
-    chiSqDistY = numpy.log10(numpy.where(chiSqDist > 1.0, chiSqDist, 1.0))
-else:
-    chiSqDistY = chiSqDist
-pyplot.plot(plotDataX, chiSqDistY)
-
-pyplot.show()
+    plotHistogram(coaddName, chiSqOrder)
