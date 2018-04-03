cc502d367a3d276826a6b76e92aa77215f147365
John K. Parejko <parejkoj@uw.edu>
2018-04-02 23:11:38 -0700

    cleanup numpydoc

--- python/lsst/coadd/chisquared/coadd.py
+++ python/lsst/coadd/chisquared/coadd.py
@@ -28,19 +28,24 @@ __all__ = ["Coadd"]
 
 
 class Coadd(coaddUtils.Coadd):
+    """Create a chi-squared coadd.
+
+    Parameters
+    ----------
+    bbox : `lsst.afw.geom.Box2I`
+        Bounding box of coadd Exposure with respect to parent:
+        coadd dimensions = bbox.getDimensions(); xy0 = bbox.getMin()
+    wcs : `lsst.afw.geom.SkyWcs`
+        WCS of coadd exposure
+    badMaskPlanes : `list` of `str`
+        Mask planes to pay attention to when rejecting masked pixels.
+        Specify as a collection of names.
+        badMaskPlanes should always include "EDGE".
+    logName : `str`, optional
+        Name by which messages are logged.
+    """
 
     def __init__(self, bbox, wcs, badMaskPlanes, logName="coadd.chisquared.Coadd"):
-        """Create a chi-squared coadd
-
-        Inputs:
-        @param[in] bbox: bounding box of coadd Exposure with respect to parent (afwGeom.Box2I):
-            coadd dimensions = bbox.getDimensions(); xy0 = bbox.getMin()
-        @param[in] wcs: WCS of coadd exposure (lsst.afw.geom.SkyWcs)
-        @param[in] badMaskPlanes: mask planes to pay attention to when rejecting masked pixels.
-            Specify as a collection of names.
-            badMaskPlanes should always include "EDGE".
-        @param[in] logName: name by which messages are logged
-        """
         coaddUtils.Coadd.__init__(self,
                                   bbox=bbox,
                                   wcs=wcs,
@@ -51,16 +56,24 @@ class Coadd(coaddUtils.Coadd):
     def addExposure(self, exposure, weightFactor=1.0):
         """Add a an exposure to the coadd; it is assumed to have the same WCS as the coadd
 
-        Inputs:
-        @param[in] exposure: Exposure to add to coadd; this must be:
+        Parameters
+        ----------
+        exposure : `lsst.afw.image.Exposure`
+            Exposure to add to coadd; this must be:
             - background-subtracted or background-matched to the other images being coadded
             - psf-matched to the desired PSF model (optional)
             - warped to match the coadd
-        @param[in] weightFactor: weight with which to add exposure to coadd
+        weightFactor : `float`
+            weight with which to add exposure to coadd
 
-        @return
-        - overlapBBox: region of overlap between exposure and coadd in parent coordinates (afwGeom.Box2I)
-        - weight: weight with which exposure was added to coadd; weight = weightFactor for this kind of coadd
+        Returns
+        -------
+        overlapBBox : `lsst.afw.geom.Box2I`
+            Region of overlap between ``exposure`` and coadd in parent
+            coordinates.
+        weight : `float`
+            Weight with which ``exposure`` was added to coadd;
+            weight = weightFactor for this kind of coadd.
         """
         self._log.info("add exposure to coadd")
 
